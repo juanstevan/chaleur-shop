6d786207cd032e6f4d2022146914f7fa59ccdac0
Juan Stevan <juanstevan@live.com>
2025-12-08 12:43:37 -0500

    Add Griddles page Add Fridges page Fix menu problem

--- menu.js
+++ menu.js
@@ -1,11 +1,14 @@
 const sandwich = document.querySelector('.sandwich');
 const menu = document.querySelector('nav');
+const main = document.querySelector('main');
 const close = document.querySelector('.close');
 
 sandwich.addEventListener('click', (e) => {
     menu.style.display = 'flex';
+    main.style.display = 'none';
 });
 
 close.addEventListener('click', (e) => {
     menu.style.display = 'none';
+    main.style.display = 'flex';
 });
